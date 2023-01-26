d692aabc2be1800b1deaa63fc5c02f88a136e64b
Noor Mohammad Gagguturi <85073575+noor-sec@users.noreply.github.com>
2023-01-26 09:28:29 -0800

    Update lion.sh

--- lion.sh
+++ lion.sh
@@ -45,8 +45,9 @@ echo
 / ___|| |_ __ _ _ __| |_  \ \      / (_) |_| |__   |  _ \ ___  ___ ___  _ __  
 \___ \| __/ _` | '__| __|  \ \ /\ / /| | __| '_ \  | |_) / _ \/ __/ _ \| '_ \ 
  ___) | || (_| | |  | |_    \ V  V / | | |_| | | | |  _ <  __/ (_| (_) | | | |
-|____/ \__\__,_|_|   \__|    \_/\_/  |_|\__|_| |_| |_| \_\___|\___\___/|_| |_|
-Finding SubDomains,Parameters,Screenshoting,OperatingSystem,Servers,ServicesRunning,WAFs,OpenPorts,Headers
+|____/ \__\__,_|_|   \__|    \_/\_/  |_|\__|_| |_| |_| \_\___|\___\___/|_| |_|"
+
+echo "Finding SubDomains,Parameters,Screenshoting,OperatingSystem,Servers,ServicesRunning,WAFs,OpenPorts,Headers
 BreachedData,WhoIs etc.,"
 
 #---------------------------------------------------------------------------------
