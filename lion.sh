cdb6e00c757ca9f4c87c7227e0fc7a5bf3c11388
Noor Mohammad Gagguturi <85073575+noor-sec@users.noreply.github.com>
2023-01-26 06:34:38 -0800

    Update lion.sh

--- lion.sh
+++ lion.sh
@@ -24,6 +24,10 @@ if [ ! -d "$target/subs-vuln" ]; then
           mkdir $target/subs_vuln
 fi
 
+if [ ! -d "$target/recon/false-positive" ]; then
+          mkdir $target/recon/false-positive
+fi
+
 if [ ! -d "$target/subs-vuln/false-positive" ]; then
           mkdir $target/subs_vuln/false_positive
 fi
@@ -54,7 +58,7 @@ cat $target/recon/subfinder.txt | grep $target >> $target/recon/final.txt
 rm $target/recon/subfinder.txt
 
 echo "[+]Enumurating SubDomains Using Findomain..." 
-findomain -t $target -q >> $url/recon/findomain.txt
+findomain -t $target -q >> $target/recon/findomain.txt
 cat $target/recon/findomain.txt | grep $target >> $target/recon/final.txt
 rm $target/recon/findomain.txt
 
@@ -64,29 +68,55 @@ cat $target/recon/sublist3r.txt | grep $target >> $target/recon/final.txt
 rm $1/recon/sublist3r.txt 
 
 echo "[+]Filtering Repeated Domains........." 
-cat $target/recon/final.txt | sort -u | tee $target/recon/final_subs.txt 
+cat $target/recon/final.txt | sort -u | tee $target/recon/final-subs.txt 
 rm $target/recon/final.txt 
 
 echo "[+]Total Unique SubDomains" 
-cat $target/recon/final_subs.txt | wc -l
+cat $target/recon/final-subs.txt | wc -l
 #--------------------------------------------------------------------------------------------------
 #-----------------------------------Filtering Live SubDomains--------------------------------------
 #--------------------------------------------------------------------------------------------------
 echo "[+]Removing Dead Domains Using httpx....." 
-cat $target/recon/final_subs.txt | httpx --silent  >> $target/recon/live_check.txt
+cat $target/recon/final-subs.txt | httpx --silent  >> $target/recon/live_check.txt
 
 echo "[+]Removing Dead Domains Using httprobe....." 
-cat $target/recon/final_subs.txt | httprobe >> $target/recon/live_check.txt
+cat $target/recon/final-subs.txt | httprobe >> $target/recon/live_check.txt
 
 echo "[+]Analyzing Both httpx & httprobe....."
-cat $target/recon/live_check.txt | sed 's/https\?:\/\///' | sort -u | tee $target/recon/live_subs.txt 
+cat $target/recon/live_check.txt | sed 's/https\?:\/\///' | sort -u | tee $target/recon/live-subs.txt 
+cat $target/recon/live_check.txt | sort -u | tee $target/recon/false-positive/https-subs.txt
 rm $target/recon/live_check.txt
 
 echo "[+]Total Unique Live SubDomains....."
-cat $target/recon/live_subs.txt | wc -l
+cat $target/recon/live-subs.txt | wc -l
+
+#--------------------------------------------------------------------------------------------------
+#----------------------------------------Enumurating Urls-----------------------------------------
+#--------------------------------------------------------------------------------------------------
+echo "[+]Enumurating Params From Paramspider...." 
+python3 /opt/Paramspider/paramspider.py --level high -d $target -p noor -o $1/recon/urls.txt
+echo "[+]Enumurating Params From Waybackurls...." 
+cat $1/recon/live-subs.txt | waybackurls | grep = | qsreplace lion >> $1/recon/urls.txt
+echo "[+]Enumurating Params From gau Tool...." 
+gau --subs  $target | grep = | qsreplace lion >> $target/recon/urls.txt 
+echo "[+]Enumurating Params From gauPlus Tool...." 
+cat $target/recon/live-subs.txt | gauplus | grep = | qsreplace lion >> $1/recon/urls.txt
+echo "[+]Enumurating Params Using Katana..."
+katana -u $target/recon/live-subs.txt | grep = | qsreplace noor >> $target/recon/urls.txt
 
 
+echo "[+]Filtering Dups..." 
+cat $1/recon/urls.txt | sort -u | tee $1/recon/final-params.txt 
+rm $target/recon/urls.txt
 
+echo "[+]Total Unique Params Found...." 
+cat $target/recon/final-params.txt | wc -l
+
+#--------------------------------------------------------------------------------------------------
+#-------------------------------Checking For HTMLi && RXSS-----------------------------------------
+#--------------------------------------------------------------------------------------------------
+echo "[+]Fuzzing For HTML Injection...." 
+cat $target/recon/final-params.txt | qsreplace '"><u>hyper</u>' | tee $target/recon/temp.txt && cat $target/recon/temp.txt | while read host do ; do curl --silent --path-as-is --insecure "$host" | grep -qs "><u>hyper</u>" && echo "$host "; done > $target/params-vuln/htmli.txt
 
 
 
